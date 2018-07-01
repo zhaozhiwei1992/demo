0b355e7349197a34e46210aa41ad72d1714a840a
zhao <347061329@qq.com>
2018-07-01 09:42:50 +0800

    注释

--- dubbo/order/order-provider/src/main/java/com/lx/demo/DatabaseConnect.java
+++ dubbo/order/order-provider/src/main/java/com/lx/demo/DatabaseConnect.java
@@ -2,11 +2,14 @@ package com.lx.demo;
 
 import java.util.ServiceLoader;
 
+/**
+ * 自定义spi测试
+ */
 public class DatabaseConnect {
     public static void main(String[] args) {
-//        ServiceLoader<DatabaseDriver> load = ServiceLoader.load(DatabaseDriver.class);
-//        for (DatabaseDriver databaseDriver : load) {
-//            System.out.println(databaseDriver.connect("localhost"));
-//        }
+        ServiceLoader<DatabaseDriver> load = ServiceLoader.load(DatabaseDriver.class);
+        for (DatabaseDriver databaseDriver : load) {
+            System.out.println(databaseDriver.connect("localhost"));
+        }
     }
 }
--- dubbo/user/src/main/java/com/lx/demo/App.java
+++ dubbo/user/src/main/java/com/lx/demo/App.java
@@ -15,25 +15,26 @@ public class App
 {
     public static void main( String[] args )
     {
-//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-//                new String[]{"META-INF/spring/order-consumer.xml"});
-//        context.start();
-//        //访问到服务端提供的实现
-//        IOrderService iOrderService = (IOrderService) context.getBean("orderService");
-//        DoOrderRequest doOrderRequest = new DoOrderRequest();
-//        doOrderRequest.setName("zhao");
-//        DoOrderResponse doOrderResponse = iOrderService.doOrder(doOrderRequest);
-//        System.out.println("客户端 user输出: ==== " + doOrderResponse);
-//        try {
-//            System.in.read();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                new String[]{"META-INF/spring/order-consumer.xml"});
+        context.start();
+        //访问到服务端提供的实现
+        IOrderService iOrderService = (IOrderService) context.getBean("orderService");
+        DoOrderRequest doOrderRequest = new DoOrderRequest();
+        doOrderRequest.setName("zhao");
+        DoOrderResponse doOrderResponse = iOrderService.doOrder(doOrderRequest);
+        System.out.println("客户端 user输出: ==== " + doOrderResponse);
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 //        Protocol myprotocol = ExtensionLoader.getExtensionLoader(Protocol.class).getExtension("myprotocol");
 //        System.out.println(myprotocol.getDefaultPort());
 
-        int defaultPort = ExtensionLoader.getExtensionLoader(Protocol.class).getAdaptiveExtension().getDefaultPort();
-        System.out.println(defaultPort);
+        //源码阅读入口
+        Protocol protocol = ExtensionLoader.getExtensionLoader(Protocol.class).getAdaptiveExtension();
+        System.out.println(protocol.getDefaultPort());
     }
 
 }
