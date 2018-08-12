697c079dcc64f651243e828f56667017147c72f1
lx7ly <zhaozhiweishanxi@gmail.com>
2018-08-12 19:57:06 +0800

    spring boot

--- springboot/src/main/java/com/lx/demo/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/lx/demo/springboot/SpringbootApplication.java
@@ -2,10 +2,20 @@ package com.lx.demo.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class SpringbootApplication {
 
+	@ResponseBody
+	@RequestMapping(value = "/sayHello")
+	public String sayHello(){
+		System.out.printf("Hello Boy");
+		return "";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
