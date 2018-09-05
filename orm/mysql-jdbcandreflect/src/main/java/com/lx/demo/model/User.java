2c6f5ffe4fa369a2a05be67de7dc7435f0286ed9
zhao <347061329@qq.com>
2018-09-05 14:10:57 +0800

    orm-mysql

---
+++ orm/mysql-jdbcandreflect/src/main/java/com/lx/demo/model/User.java
@@ -0,0 +1,46 @@
+package com.lx.demo.model;
+
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Table(name = "t_user")
+public class User implements Serializable {
+
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
