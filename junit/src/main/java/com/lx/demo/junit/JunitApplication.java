006fe5b334b8194cc7a4ce1b35f5158c11b4b778
lx7ly <zhaozhiweishanxi@gmail.com>
2018-09-09 13:26:01 +0800

    junit init

---
+++ junit/src/main/java/com/lx/demo/junit/JunitApplication.java
@@ -0,0 +1,12 @@
+package com.lx.demo.junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitApplication.class, args);
+	}
+}
