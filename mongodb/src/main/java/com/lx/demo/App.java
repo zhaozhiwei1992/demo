64d4934b73ef0ded52212c3b12de665aea47ab84
zhao <347061329@qq.com>
2018-09-10 16:56:17 +0800

    mongodb init

---
+++ mongodb/src/main/java/com/lx/demo/App.java
@@ -0,0 +1,13 @@
+package com.lx.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
