8a477a9a6888a2dd7fbaee0c82019cf2acb59558
zhao <347061329@qq.com>
2018-11-12 15:44:23 +0800

    xx

--- springboot/springboot-jdbc/src/main/java/com/lx/demo/springbootjdbc/webflux/WebFluxConfiguration.java
+++ springboot/springboot-jdbc/src/main/java/com/lx/demo/springbootjdbc/webflux/WebFluxConfiguration.java
@@ -1,9 +1,12 @@
 package com.lx.demo.springbootjdbc.webflux;
 
+import com.lx.demo.springbootjdbc.domain.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
@@ -19,5 +22,19 @@ public class WebFluxConfiguration {
     @Bean
     public RouterFunction<ServerResponse> saveUser(UserHandler userHandler){
         return route(POST("/webflux/user/save"), userHandler::save);
+    }
+
+    /**
+     *
+     * @param
+     * @return
+     */
+    @Bean
+    public RouterFunction<ServerResponse> findAll(){
+        return route(POST("/webflux/user/all"), (ServerRequest serverRequest)->{
+            Mono<User> userMono = serverRequest.bodyToMono(User.class);
+//            ServerResponse.ok().body()
+            return null;
+        });
     }
 }
