d7e4be3c01bb7cd7a725355c1959f1ea9c8ad23e
zhao <347061329@qq.com>
2018-11-14 16:24:53 +0800

    加入分组校验后只校验了分组，其他失效

--- springboot/springboot-validate/src/main/java/com/lx/demo/springbootvalidate/controller/UserController.java
+++ springboot/springboot-validate/src/main/java/com/lx/demo/springbootvalidate/controller/UserController.java
@@ -44,6 +44,8 @@ public class UserController {
 
     /**
      * 能不能看片看年龄 >18 可以
+     *
+     * 加入分组后只会控制 SexMovie分组，其他校验全部失效
      * <p>
      * {"timestamp":"2018-11-14T07:10:58.177+0000","status":400,"error":"Bad Request","errors":[{"codes":["Min.user.age","Min.age","Min.int","Min"],"arguments":[{"codes":["user.age","age"],"arguments":null,"defaultMessage":"age","code":"age"},18],"defaultMessage":"未成年人不能看片","objectName":"user","field":"age","rejectedValue":1,"bindingFailure":false,"code":"Min"}],"message":"Validation failed for object='user'. Error count: 1","path":"/user/sexmovie"}%
      * ➜  ~ curl -H "Accept:application/json" -H "Content-Type:application/json" -X POST -d '{"id":1,"name":"lisi","email":"11@qq.com","phone":"18212356459","age":1}' http://127.0.0.1:8080/user/sexmovie
