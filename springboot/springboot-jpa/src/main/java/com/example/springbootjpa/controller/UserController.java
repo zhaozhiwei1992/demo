3604d08d19cd7e2aa2a2c0e3654ce247453778ec
lx7ly <zhaozhiweishanxi@gmail.com>
2019-01-19 23:49:09 +0800

    user controller && use aliecs mysql

--- springboot/springboot-jpa/src/main/java/com/example/springbootjpa/controller/UserController.java
+++ springboot/springboot-jpa/src/main/java/com/example/springbootjpa/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.example.springbootjpa.controller;
 
+import com.example.springbootjpa.domain.User;
+import com.example.springbootjpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user/save")
+    public User save(@RequestBody User user){
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/user/findall")
+    public Collection<User> findAll(){
+        return userRepository.findAll();
+    }
 }
