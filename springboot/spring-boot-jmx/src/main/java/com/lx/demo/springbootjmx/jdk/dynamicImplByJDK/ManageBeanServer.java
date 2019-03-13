7f8a79155d1fe12072370cec7727ba9a0f83e155
zhao <347061329@qq.com>
2019-03-13 17:50:21 +0800

    mbean

--- springboot/spring-boot-jmx/src/main/java/com/lx/demo/springbootjmx/jdk/dynamicImplByJDK/ManageBeanServer.java
+++ springboot/spring-boot-jmx/src/main/java/com/lx/demo/springbootjmx/jdk/dynamicImplByJDK/ManageBeanServer.java
@@ -9,10 +9,14 @@ import java.lang.management.ManagementFactory;
 public class ManageBeanServer {
     public static void main(String[] args) throws Exception {
 
+        // MBean 服务器 - Agent Level
         MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
 
+        // 注册名称
         ObjectName objectName = new ObjectName("com.lx.demo.springbootjmx.jdk.dynamicImplByJDK:type=Person");
         DynamicMBean mBean = new StandardMBean(new Coder(), Person.class);
+
+        // 注册 MBean
         mBeanServer.registerMBean(mBean, objectName);
 
         System.out.println("com.lx.demo.springbootjmx.jdk.dynamicImplByJDK.ManageBeanServer staring.....");
