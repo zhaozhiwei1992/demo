b6364f766124e21b3dd4910d526f4f4634577203
zhao <347061329@qq.com>
2019-03-17 16:54:47 +0800

    springboot 整合mybatis

---
+++ springboot/springboot-mybatis/src/main/java/com/lx/demo/springbootmybatis/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.lx.demo.springbootmybatis.controller;
+
+import com.lx.demo.springbootmybatis.domain.User;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * curl http://127.0.0.1:8080/user/1
+ *
+ * 返回结果： {"id":1,"name":"admin","password":"21232f297a57a5a743894a0e4a801fc3","realname":"helloworld",
+ * "avatar":null,"mobile":"13073804251","sex":"男","status":1,"createTime":"2017-09-04T21:32:15.000+0000"}%
+ */
+@RestController
+public class UserController {
+
+    @Autowired
+    private SqlSessionTemplate sqlSessionTemplate;
+
+    @GetMapping("/user/{id}")
+    public User selectOne(@PathVariable int id){
+        User user = sqlSessionTemplate.selectOne("com.lx.demo.springbootmybatis.mapper.UserMapper.selectByPrimaryKey", 1);
+        return user;
+    }
+}
