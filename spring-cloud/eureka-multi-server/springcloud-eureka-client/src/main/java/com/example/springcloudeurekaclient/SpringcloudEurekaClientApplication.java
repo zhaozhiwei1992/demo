21be7ea025d65dab7b4bd09f3dc24c60d8d48a41
lx7ly <zhaozhiweishanxi@gmail.com>
2019-03-24 12:45:13 +0800

    eureka 多服务端

---
+++ spring-cloud/eureka-multi-server/springcloud-eureka-client/src/main/java/com/example/springcloudeurekaclient/SpringcloudEurekaClientApplication.java
@@ -0,0 +1,18 @@
+package com.example.springcloudeurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 推荐使用discovery，保持通用性，支持consul
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringcloudEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudEurekaClientApplication.class, args);
+	}
+
+}
