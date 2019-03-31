4494768b0162d8051dbdd04c707c1d2c5a9fac18
lx7ly <zhaozhiweishanxi@gmail.com>
2019-03-31 17:38:05 +0800

    hystrixdashboard 构建

--- spring-cloud/microservice/hystrix-dashboard/src/main/java/com/example/App.java
+++ spring-cloud/microservice/hystrix-dashboard/src/main/java/com/example/App.java
@@ -1,13 +1,18 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
 /**
- * Hello world!
  *
+ * 作為hystrix的dashboard
  */
+@SpringBootApplication
+@EnableHystrixDashboard
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
     }
 }
