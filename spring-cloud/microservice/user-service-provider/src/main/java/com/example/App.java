9cc76bb56aff899f4d0aaf59be944d07f422b9a4
lx7ly <zhaozhiweishanxi@gmail.com>
2019-03-31 17:38:31 +0800

    hystrix 熔断

--- spring-cloud/microservice/user-service-provider/src/main/java/com/example/App.java
+++ spring-cloud/microservice/user-service-provider/src/main/java/com/example/App.java
@@ -3,14 +3,17 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  *
  * 用户服务提供者: 用来对外提供用户服务, 类似平时写的一些service
  *  需要注册到eureka中心
+ * @author zhaoz
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class App {
     public static void main( String[] args ) {
         SpringApplication.run(App.class, args);
