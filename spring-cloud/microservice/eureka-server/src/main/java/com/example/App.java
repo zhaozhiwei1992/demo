823aad299912c812a364b42db85397893b335ec9
lx7ly <zhaozhiweishanxi@gmail.com>
2019-03-31 14:25:08 +0800

    eureka-server 构建完成

--- spring-cloud/microservice/eureka-server/src/main/java/com/example/App.java
+++ spring-cloud/microservice/eureka-server/src/main/java/com/example/App.java
@@ -1,13 +1,17 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+@EnableEurekaServer
+public class App {
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
     }
 }
