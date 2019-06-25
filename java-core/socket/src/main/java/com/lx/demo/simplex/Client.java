7c9e3a093acf00d596eefa2d27d7d7655d91acaa
lx7ly <zhaozhiweishanxi@gmail.com>
2019-06-25 23:33:36 +0800

    服务端接受多次传入， 一级为什么要用println, 涨姿势了

--- java-core/socket/src/main/java/com/lx/demo/simplex/Client.java
+++ java-core/socket/src/main/java/com/lx/demo/simplex/Client.java
@@ -5,6 +5,8 @@ import java.net.Socket;
 
 /**
  * 为甚么要用println {@link https://www.cnblogs.com/wxgblogs/p/5347996.html}
+ *
+ * println == write + flush
  */
 public class Client {
 
@@ -19,8 +21,11 @@ public class Client {
 //            BufferedReader sysBuff = new BufferedReader(new InputStreamReader(System.in));
             // 这里不用println就死翘翘了
 //            bufferedWriter.println(sysBuff.readLine());
-            bufferedWriter.println("laji ");
-//            bufferedWriter.flush();
+
+//            bufferedWriter.println("laji ");
+            // 如果用write 显示加入换行
+            bufferedWriter.write("lll222l\r\n");
+            bufferedWriter.flush();
             System.out.println("写出完成..");
 
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
