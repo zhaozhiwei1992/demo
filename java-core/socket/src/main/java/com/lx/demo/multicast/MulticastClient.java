5bd06eed28c49496e656c6df24fa5f66b519df91
lx7ly <zhaozhiweishanxi@gmail.com>
2019-06-26 21:41:08 +0800

    通过guava中便利类线程池创建线程

--- java-core/socket/src/main/java/com/lx/demo/multicast/MulticastClient.java
+++ java-core/socket/src/main/java/com/lx/demo/multicast/MulticastClient.java
@@ -1,10 +1,13 @@
 package com.lx.demo.multicast;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.UnknownHostException;
+import java.util.concurrent.*;
 
 /**
  * 启动多个线程接受组播发送消息
@@ -12,15 +15,32 @@ import java.net.UnknownHostException;
 public class MulticastClient {
 
     public static void main(String[] args) {
+
+        /**
+         * public ThreadPoolExecutor(int corePoolSize,
+         *                               int maximumPoolSize,
+         *                               long keepAliveTime,
+         *                               TimeUnit unit,
+         *                               BlockingQueue<Runnable> workQueue)
+         */
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                10,
+                15,
+                10,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue(1024),
+                new ThreadFactoryBuilder().setNameFormat("guava-%d").build(),
+                new ThreadPoolExecutor.DiscardPolicy()
+        );
         //启动10个客户端来读取消息
-        for (int i = 0; i < 5; i++) {
-            new Thread(()->{
-                new MulticastClient().muticastClientSub();
-            }).start();
+        for (int i = 0; i < 20; i++) {
+//            new Thread(()-> muticastClientSub()).start();
+//            Executors.newSingleThreadExecutor().execute(MulticastClient ::muticastClientSub);
+            executor.execute(MulticastClient::muticastClientSub);
         }
     }
 
-    private void muticastClientSub() {
+    private static void muticastClientSub() {
         //加入到指定组
         try {
             InetAddress group = InetAddress.getByName("224.5.6.7");
