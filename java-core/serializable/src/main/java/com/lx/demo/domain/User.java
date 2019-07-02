3650e757b177498e5d20f8f0b966d226187b403d
lx7ly <zhaozhiweishanxi@gmail.com>
2019-07-02 21:35:03 +0800

    简单序列化demo

--- java-core/serializable/src/main/java/com/lx/demo/domain/User.java
+++ java-core/serializable/src/main/java/com/lx/demo/domain/User.java
@@ -3,11 +3,38 @@ package com.lx.demo.domain;
 import java.io.Serializable;
 
 
+/**
+ * 序列化对象
+ */
 public class User implements Serializable {
 
     private static final long serialVersionUID = 2083262352962630020L;
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
     private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     private String name;
 }
---
+++ java-core/serializable/src/main/java/com/lx/demo/javacore/SerializeDemo.java
@@ -0,0 +1,67 @@
+package com.lx.demo.javacore;
+
+import com.lx.demo.domain.User;
+
+import java.io.*;
+
+/**
+ * javacore 序列化 反序列化实现
+ */
+public class SerializeDemo {
+
+    public static void main(String[] args) {
+
+        User user = new User();
+        user.setId(1L);
+        user.setName("zhangsan");
+        String serializePath = new SerializeDemo().serialize(user);
+
+        // 这里修改后也不影响序列化
+        user.setName("lisi");
+        User user1 = new SerializeDemo().deSerialize(serializePath);
+        System.out.println(user1);
+    }
+
+    /**
+     * 序列化
+     * ObjectOutputStream 类用来序列化一个对象
+     * 注意： 当序列化一个对象到文件时， 按照 Java 的标准约定是给文件一个 .ser 扩展名
+     * @param user
+     */
+    public String serialize(User user){
+        String path = System.getProperty("user.dir");
+        String separator = File.separator;
+        path += (separator + "serializable" + separator + "user.ser");
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(path);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(user);
+            System.out.println("serialize in user.ser");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return path;
+    }
+
+    /**
+     * 反序列化
+     * @return
+     */
+    public User deSerialize(String path){
+        try {
+            FileInputStream fileInputStream = new FileInputStream(path);
+            ObjectInputStream inputStream = new ObjectInputStream(fileInputStream);
+            Object readObject = inputStream.readObject();
+            return (User) readObject;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
