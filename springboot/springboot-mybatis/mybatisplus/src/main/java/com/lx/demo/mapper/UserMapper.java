c6ff23998748bc530c9d17274b2bc4ace7fb7f02
lx7ly <zhaozhiweishanxi@gmail.com>
2019-07-27 13:39:46 +0800

    mybatisplus demo 带分页

---
+++ springboot/springboot-mybatis/mybatisplus/src/main/java/com/lx/demo/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.lx.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lx.demo.domain.User;
+
+/**
+ * @author zhaoz
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
