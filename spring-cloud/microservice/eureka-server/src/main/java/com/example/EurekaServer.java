fba495c416519ea7e4ff1f2d0d0284ba8445e5b4
zhao <347061329@qq.com>
2019-07-30 23:14:38 +0800

    eureka注册中心改造，支持高可用

--- spring-cloud/microservice/eureka-server/src/main/java/com/example/EurekaServer.java
+++ spring-cloud/microservice/eureka-server/src/main/java/com/example/EurekaServer.java
@@ -6,12 +6,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Hello world!
- *
+ * 如果启动集群　eureka 假如启动参数
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class App {
+public class EurekaServer {
     public static void main( String[] args ) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(EurekaServer.class, args);
     }
 }
