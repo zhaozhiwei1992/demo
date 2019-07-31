786142799586af3d3a4089e57bfd923c58dff3c1
zhao <347061329@qq.com>
2019-08-01 00:14:21 +0800

    user-service-client加入客户端负载均衡

--- spring-cloud/microservice/user-service-client/src/main/java/com/example/UserServiceClientMain.java
+++ spring-cloud/microservice/user-service-client/src/main/java/com/example/UserServiceClientMain.java
@@ -3,7 +3,10 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  *
@@ -13,8 +16,18 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class App {
+public class UserServiceClientMain {
     public static void main( String[] args ) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(UserServiceClientMain.class, args);
+    }
+
+    /**
+     * 当前服务resttemplate访问加入负载均衡
+     * @return
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
