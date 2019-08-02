3d347cd297e852960e4ebc8798e23578af01091c
zhao <347061329@qq.com>
2019-08-03 00:22:52 +0800

    xx

--- spring-cloud/microservice/user-service-provider/src/main/java/com/example/intercepter/CustomIntercepter.java
+++ spring-cloud/microservice/user-service-provider/src/main/java/com/example/intercepter/CustomIntercepter.java
@@ -2,14 +2,10 @@ package com.example.intercepter;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Collection;
 
 /**
  * 通过拦截器输出登录信息
