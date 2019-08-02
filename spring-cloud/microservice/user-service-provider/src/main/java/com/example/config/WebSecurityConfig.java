cc679fc5244c83a5ee52cbeef113a18c55806017
zhao <347061329@qq.com>
2019-08-02 22:30:23 +0800

    设置忽略验证url

--- spring-cloud/microservice/user-service-provider/src/main/java/com/example/config/WebSecurityConfig.java
+++ spring-cloud/microservice/user-service-provider/src/main/java/com/example/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
@@ -56,6 +57,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(this.userDetailService).passwordEncoder(this.passwordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // 配置忽略验证的url
+        web.ignoring().antMatchers("/users/**");
     }
 
     @Component
