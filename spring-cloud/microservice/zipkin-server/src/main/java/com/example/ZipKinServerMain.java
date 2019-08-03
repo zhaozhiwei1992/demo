a055ef47d14a45210355959a706f2655f7ab87a0
zhao <347061329@qq.com>
2019-08-03 20:24:45 +0800

    zipkin server

---
+++ spring-cloud/microservice/zipkin-server/src/main/java/com/example/ZipKinServerMain.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class ZipKinServerMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinServerMain.class);
+    }
+}
