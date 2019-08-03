05975279bffa32d4d4dd3ae147e205afaedee998
zhao <347061329@qq.com>
2019-08-03 21:09:12 +0800

    zipkin server, 没有监控到x

--- spring-cloud/microservice/zipkin-server/src/main/java/com/example/ZipKinServerMain.java
+++ spring-cloud/microservice/zipkin-server/src/main/java/com/example/ZipKinServerMain.java
@@ -2,11 +2,15 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin.server.internal.EnableZipkinServer;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZipkinServer
 public class ZipKinServerMain {
     public static void main(String[] args) {
         SpringApplication.run(ZipKinServerMain.class);
