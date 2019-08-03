72801727d8a42d20b8ee86367b6128cc7aa27096
zhao <347061329@qq.com>
2019-08-03 10:41:35 +0800

    通过rabbitmq来接收hystrix数据

--- spring-cloud/microservice/netflix-turbine/src/main/java/com/example/TurbineMain.java
+++ spring-cloud/microservice/netflix-turbine/src/main/java/com/example/TurbineMain.java
@@ -2,13 +2,15 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+//import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
-@EnableTurbine
+//@EnableTurbine
+@EnableTurbineStream
 public class TurbineMain {
     public static void main(String[] args) {
         SpringApplication.run(TurbineMain.class);
