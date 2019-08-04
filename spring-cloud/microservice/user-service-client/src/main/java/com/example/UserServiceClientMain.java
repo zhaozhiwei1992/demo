bf06e7b259f6368823a31bbfebed78d9be9b6366
zhao <347061329@qq.com>
2019-08-04 19:06:47 +0800

    eureka高可用后保证各服务正常

--- spring-cloud/microservice/user-service-client/src/main/java/com/example/UserServiceClientMain.java
+++ spring-cloud/microservice/user-service-client/src/main/java/com/example/UserServiceClientMain.java
@@ -1,14 +1,11 @@
 package com.example;
 
-import com.example.config.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.web.client.RestTemplate;
 
 /**
