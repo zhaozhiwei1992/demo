26227b49f19ce8dc60df225b70ccd73f5e906d69
zhao <347061329@qq.com>
2019-08-08 16:07:31 +0800

    thymeleaf layout测试

---
+++ springboot/springboot-thymeleaf/springboot-thymeleaf-layout/src/main/java/com/lx/demo/springbootthymeleaflayout/web/controller/IndexController.java
@@ -0,0 +1,30 @@
+package com.lx.demo.springbootthymeleaflayout.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/fragment")
+    public String fragment() {
+        return "fragment";
+    }
+
+    @RequestMapping("/layout")
+    public String layout() {
+        return "layout";
+    }
+
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+
+}
