f99ea076675939bee087199945db420cfb846adb
zhao <347061329@qq.com>
2019-08-12 15:12:16 +0800

    邮件传输简单文本

--- springboot/springboot-mail/src/test/java/com/lx/demo/springbootmail/service/SimpleMailServiceTest.java
+++ springboot/springboot-mail/src/test/java/com/lx/demo/springbootmail/service/SimpleMailServiceTest.java
@@ -3,6 +3,7 @@ package com.lx.demo.springbootmail.service;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,8 +16,11 @@ public class SimpleMailServiceTest {
     @Autowired
     private SimpleMailService simpleMailService;
 
+    @Value("${mail.to.addr}")
+    private String mailto;
+
     @Test
     public void sendMail() {
-        simpleMailService.sendMail("test", "347061329@qq.com", "test");
+        simpleMailService.sendMail("test", mailto, "test");
     }
 }
