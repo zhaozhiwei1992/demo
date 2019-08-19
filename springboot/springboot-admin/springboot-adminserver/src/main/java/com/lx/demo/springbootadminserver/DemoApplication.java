7e0c8914de79073d614e4b4031020ba54fba6173
zhao <347061329@qq.com>
2019-08-19 22:04:00 +0800

    springboot admin

---
+++ springboot/springboot-admin/springboot-adminserver/src/main/java/com/lx/demo/springbootadminserver/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.lx.demo.springbootadminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
