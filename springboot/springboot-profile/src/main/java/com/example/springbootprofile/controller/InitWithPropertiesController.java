3af7501b2300eff784aa024391a7b0076cf1bb98
zhao <347061329@qq.com>
2019-08-22 23:43:31 +0800

    自动读取文件并配置属性

--- springboot/springboot-profile/src/main/java/com/example/springbootprofile/controller/InitWithPropertiesController.java
+++ springboot/springboot-profile/src/main/java/com/example/springbootprofile/controller/InitWithPropertiesController.java
@@ -1,5 +1,6 @@
 package com.example.springbootprofile.controller;
 
+import com.example.springbootprofile.domain.InitWithCustomProperties;
 import com.example.springbootprofile.domain.InitWithProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,6 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableConfigurationProperties(InitWithProperties.class)
 public class InitWithPropertiesController {
+
+    @Autowired
+    private InitWithCustomProperties initWithCustomProperties;
+
+    /**
+     * curl -X GET http://localhost:8080/email/properties
+     * {"from":"xx","to":"oo"}%
+     * @return
+     */
+    @GetMapping("/email/properties")
+    public InitWithCustomProperties getInitWithCustomProperties(){
+        return initWithCustomProperties;
+    }
 
     @Autowired
     private InitWithProperties initWithProperties;
---
+++ springboot/springboot-profile/src/main/java/com/example/springbootprofile/domain/InitWithCustomProperties.java
@@ -0,0 +1,29 @@
+package com.example.springbootprofile.domain;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix="email")
+@PropertySource(value={"classpath:email.properties"})
+public class InitWithCustomProperties {
+    private String from;
+    private String to;
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+}
