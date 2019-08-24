4d6fc5c88d7dc8287adb19585bc358b9d978e7a7
zhao <347061329@qq.com>
2019-08-24 09:52:45 +0800

    springboot logback

--- springboot/springlog/src/main/java/com/lx/demo/springbootlog/web/rest/IndexResource.java
+++ springboot/springlog/src/main/java/com/lx/demo/springbootlog/web/rest/IndexResource.java
@@ -1,13 +1,17 @@
 package com.lx.demo.springbootlog.web.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexResource {
+    private static final Logger logger = LoggerFactory.getLogger(IndexResource.class);
 
     @GetMapping("/")
     public String index(){
+        logger.debug("{}", Thread.currentThread().getStackTrace()[1].getMethodName());
         return "xx";
     }
 }
