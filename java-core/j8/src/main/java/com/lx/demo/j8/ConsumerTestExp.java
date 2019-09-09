85fc0d6bd4c408b2b6482ef1d2299e91a734ad35
zhao <347061329@qq.com>
2019-09-09 13:56:55 +0800

    玩转j8

--- java-core/j8/src/main/java/com/lx/demo/j8/ConsumerTestExp.java
+++ java-core/j8/src/main/java/com/lx/demo/j8/ConsumerTestExp.java
@@ -8,6 +8,8 @@ import java.util.stream.Stream;
 
 /**
  * consumer是j8提供的一种模板方式，不要去继承, 只要符合规范都可以是consumer
+ *
+ * 只进不出
  */
 public class ConsumerTestExp {
 
@@ -25,5 +27,19 @@ public class ConsumerTestExp {
         Arrays.asList(1, 2, 3, 4, 5).forEach(System.out::println);
         //上面为该方式的简写形式
         Arrays.asList(1, 2, 3, 4, 5).forEach((Integer i) -> System.out.println(i));
+
+        //and then
+        // 这里输出了四次，
+        Consumer<String> consumerStr = System.out::println;
+        consumerStr.accept("hello"); //hello
+        consumerStr // world
+                .andThen(consumerStr) //world
+                .andThen(ConsumerTestExp::echo) //echo world
+                .accept("world");
     }
+
+    public static void echo(String msg){
+        System.out.printf("echo %s: \n", msg);
+    }
+
 }
