41f80db0fc3e67154436addd43c16ee71159c99b
lx7ly <zhaozhiweishanxi@gmail.com>
2019-09-19 20:13:36 +0800

    防止整体repackage提示找不到主类

---
+++ spring-cloud/microservice/custom-monitor/src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ * 就是为了凑数，防止repackage提示找不到mainclass
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
