e808d6e2196fc70a31545568e4563f3e44c2caa1
lx7ly <zhaozhiweishanxi@gmail.com>
2019-09-21 22:06:11 +0800

    optional涨姿势

--- java-core/j8/src/main/java/com/lx/demo/j8/OptionalExp.java
+++ java-core/j8/src/main/java/com/lx/demo/j8/OptionalExp.java
@@ -1,5 +1,6 @@
 package com.lx.demo.j8;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 /**
@@ -10,8 +11,35 @@ import java.util.Optional;
 public class OptionalExp {
 
     public static void main(String[] args) {
+
+        //optional中的flutte风格
+        final Optional<String> s = getTestOptional().flatMap(TestOptional::hello);
+        // ==> 等价
+        final Optional<String> hello = getTestOptional().get().hello();
+
+        //有参数更刺激, 通过optional的方式求一个数的平方根
+        //普通方式
+        final Double aDouble = revers(4.0).get();
+        final Optional<Double> squareRoot = OptionalExp.squareRoot(aDouble);
+        System.out.println(squareRoot.get());
+
+        //文艺方式, stream中的flatMap和optional中类似，可以把optional理解为带有一个value的stream, 那么flatMap就相当于是
+        //把内部optional的value拿出来撸, 就像脱掉了optional或者stream的外衣，只留下内部的操作
+        final Optional<Double> squareRoot2 =
+                Optional.of(4.0).flatMap(OptionalExp::revers).flatMap(OptionalExp::squareRoot);
+        System.out.printf("优雅方式获取4.0的平方根 %s\n", squareRoot2.get());
+
+
+        //创建optional
+        Optional<Double> num = revers(1.0);
+        System.out.println(num.get());
+
+        num.ifPresent(n -> {
+            System.out.printf("如果有值计算 %s \n ", n+2.0);
+        });
+
         Integer value1 = null;
-        Integer value2 = new Integer(10);
+        Integer value2 = Integer.valueOf(10);
 
         // Optional.ofNullable - 允许传递为 null 参数
         Optional<Integer> a = Optional.ofNullable(value1);
@@ -19,8 +47,46 @@ public class OptionalExp {
         // Optional.of - 如果传递的参数是 null，抛出异常 NullPointerException
         Optional<Integer> b = Optional.of(value2);
 
-        Integer intA = a.orElse(new Integer(0));
+//        Exception in thread "main" java.util.NoSuchElementException
+//        a.orElseThrow(NoSuchElementException::new);
+        Integer intA = a.orElse(Integer.valueOf(0));
+        //需要时候调用
+        Integer intAGet = a.orElseGet(() -> Integer.valueOf(0));
         Integer intB = b.get();
-        System.out.println(intA + intB);
+        System.out.println(intA + intB + intAGet);
+    }
+
+    /**
+     * 返回一个包装testoptional对象的optional
+     * @return
+     */
+    public static Optional<TestOptional> getTestOptional(){
+        return Optional.of(new TestOptional());
+    }
+
+    /**
+     * 将一个double对象转换为optional
+     * @param v
+     * @return
+     */
+    private static Optional<Double> revers(Double v) {
+        return v==0?Optional.empty():Optional.of(v);
+    }
+
+    /**
+     * 平方根
+     * @param v
+     * @return
+     */
+    private static Optional<Double> squareRoot(Double v) {
+        return v < 0?Optional.empty() : Optional.of(Math.sqrt(v));
+    }
+}
+
+class TestOptional{
+    public Optional<String> hello(){
+        final String methodName = Thread.currentThread().getStackTrace()[0].getMethodName();
+        System.out.printf("请求到了 %s\n", methodName);
+        return Optional.of(methodName);
     }
 }
