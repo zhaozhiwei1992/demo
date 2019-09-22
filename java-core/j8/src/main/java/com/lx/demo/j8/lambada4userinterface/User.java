016acb74d7cc2550e0d6d557795c4ebed5c6e8b8
lx7ly <zhaozhiweishanxi@gmail.com>
2019-09-22 22:20:43 +0800

    吊炸天聚合函数collec以及它的小伙伴们

--- java-core/j8/src/main/java/com/lx/demo/j8/lambada4userinterface/User.java
+++ java-core/j8/src/main/java/com/lx/demo/j8/lambada4userinterface/User.java
@@ -1,6 +1,24 @@
 package com.lx.demo.j8.lambada4userinterface;
 
 public class User {
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private Long id;
+    private int age;
     public String getName() {
         return name;
     }
@@ -11,6 +29,15 @@ public class User {
 
     private String name;
 
+    public User() {
+    }
+
+    public User(Long id, int age, String name) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+
     public static User getUserByName(String name){
         User user = new User();
         user.setName("ligoudan");
@@ -20,7 +47,9 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
-                "name='" + name + '\'' +
+                "id=" + id +
+                ", age=" + age +
+                ", name='" + name + '\'' +
                 '}';
     }
 }
