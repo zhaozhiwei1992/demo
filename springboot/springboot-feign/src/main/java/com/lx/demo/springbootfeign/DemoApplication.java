b2f7099997933372d4b92c326f5ed7f3f05bccde
zhao <347061329@qq.com>
2019-11-06 15:08:35 +0800

    springboot feign 测试

---
+++ springboot/springboot-feign/src/main/java/com/lx/demo/springbootfeign/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.lx.demo.springbootfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
