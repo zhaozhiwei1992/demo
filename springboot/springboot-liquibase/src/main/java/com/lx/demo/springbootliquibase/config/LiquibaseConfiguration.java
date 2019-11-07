b475bbd3cba8d8ea101528ca555464ecb23692e7
lx7ly <zhaozhiweishanxi@gmail.com>
2019-11-07 22:44:47 +0800

    liquibase根据sql版本升级，只能递增，升级过的脚本不能再升级

--- springboot/springboot-liquibase/src/main/java/com/lx/demo/springbootliquibase/config/LiquibaseConfiguration.java
+++ springboot/springboot-liquibase/src/main/java/com/lx/demo/springbootliquibase/config/LiquibaseConfiguration.java
@@ -4,13 +4,20 @@ import liquibase.integration.spring.SpringLiquibase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
+/**
+ * 只有使用了enableconfigurationproperties
+ *  为带有@ConfigurationProperties注解的Bean提供有效的支持。这个注解可以提供一种方便的方式来将带有@ConfigurationProperties注解的类注入为Spring容器的Bean
+ */
 @Configuration
+@EnableConfigurationProperties(LiquibaseProperties.class)
 public class LiquibaseConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(LiquibaseConfiguration.class);
@@ -26,14 +33,12 @@ public class LiquibaseConfiguration {
             LiquibaseProperties liquibaseProperties,
             DataSource dataSource) {
 
-        // If you don't want Liquibase to start asynchronously, substitute by this:
-        // SpringLiquibase liquibase = SpringLiquibaseUtil.createSpringLiquibase(liquibaseDataSource.getIfAvailable()
-        // , liquibaseProperties, dataSource.getIfUnique(), dataSourceProperties);
         SpringLiquibase liquibase = new SpringLiquibase();
         liquibase.setChangeLog("classpath:config/liquibase/master.xml");
         liquibase.setDataSource(dataSource);
         liquibase.setContexts("development,test,production");
         liquibase.setShouldRun(liquibaseProperties.isEnabled());
+//        liquibase.setShouldRun(true);
         log.debug("Configuring Liquibase");
         return liquibase;
     }
