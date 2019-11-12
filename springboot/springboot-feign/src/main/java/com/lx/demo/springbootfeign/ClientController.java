4dbdce6c3a20934e8e788cc385f05c0a1c0923fc
离线请留言 <347061329@qq.com>
2019-11-12 10:05:31 +0800

    ip default value test

--- springboot/springboot-feign/src/main/java/com/lx/demo/springbootfeign/ClientController.java
+++ springboot/springboot-feign/src/main/java/com/lx/demo/springbootfeign/ClientController.java
@@ -1,6 +1,9 @@
 package com.lx.demo.springbootfeign;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClientController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ClientController.class);
+
     @Autowired
     private HelloClientFeign feignClient;
+
+    @Value("${application.testip:127.0.0.1:9999}")
+    private String ip;
+
+    @GetMapping("/testip")
+    public String testIp(){
+        logger.info("testip {}", ip);
+        return ip;
+    }
 
     @GetMapping("/hello")
     public String hello(@RequestParam String name){
