1a01835a89c48e6d32f38a34d9dbea3e7187811e
离线请留言 <347061329@qq.com>
2019-12-28 20:50:20 +0800

    easy excel demo https://alibaba-easyexcel.github.io/quickstart/read.html

---
+++ springboot/springboot-easyexcel/src/test/java/com/lx/demo/springbooteasyexcel/WriteTest.java
@@ -0,0 +1,70 @@
+package com.lx.demo.springbooteasyexcel;
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
+import com.lx.demo.springbooteasyexcel.domain.DemoData;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.ResourceUtils;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 写的常见写法
+ *
+ * @author Jiaju Zhuang
+ */
+public class WriteTest {
+    /**
+     * 最简单的写
+     * <p>
+     * 1. 创建excel对应的实体对象 参照{@link DemoData}
+     * <p>
+     * 2. 直接写即可
+     */
+    @Test
+    public void simpleWrite() throws FileNotFoundException {
+        // 写法1
+        String fileName = ResourceUtils.getFile("classpath").getPath() + "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 如果这里想使用03 则 传入excelType参数即可
+        EasyExcel.write(fileName, DemoData.class).sheet("模板").doWrite(data());
+
+        // 写法2
+        fileName = ResourceUtils.getFile("classpath").getPath() + "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写
+        ExcelWriter excelWriter = EasyExcel.write(fileName, DemoData.class).build();
+        WriteSheet writeSheet = EasyExcel.writerSheet("模板").build();
+        excelWriter.write(data(), writeSheet);
+        /// 千万别忘记finish 会帮忙关闭流
+        excelWriter.finish();
+    }
+
+    private List<List<Object>> dataList() {
+        List<List<Object>> list = new ArrayList<List<Object>>();
+        for (int i = 0; i < 10; i++) {
+            List<Object> data = new ArrayList<Object>();
+            data.add("字符串" + i);
+            data.add(new Date());
+            data.add(0.56);
+            list.add(data);
+        }
+        return list;
+    }
+
+    private List<DemoData> data() {
+        List<DemoData> list = new ArrayList<DemoData>();
+        for (int i = 0; i < 10; i++) {
+            DemoData data = new DemoData();
+            data.setString("字符串" + i);
+            data.setDate(new Date());
+            data.setDoubleData(0.56);
+            list.add(data);
+        }
+        return list;
+    }
+
+}
