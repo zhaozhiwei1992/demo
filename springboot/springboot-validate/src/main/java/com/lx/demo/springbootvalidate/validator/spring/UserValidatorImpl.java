90ceb8a4992f8e58bfaa6c4c9fd47558e24ca6d0
离线请留言 <347061329@qq.com>
2019-12-28 17:15:03 +0800

    delete .idea

--- springboot/springboot-validate/src/main/java/com/lx/demo/springbootvalidate/validator/spring/UserValidatorImpl.java
+++ springboot/springboot-validate/src/main/java/com/lx/demo/springbootvalidate/validator/spring/UserValidatorImpl.java
@@ -9,6 +9,8 @@ import org.springframework.validation.Validator;
 
 /**
  * 通过spring的方式校验属性
+ * 不需要在bean里写任何注解,耦合读很低, 如果注解方式能力不够，不想写大量自定义注解，这个更靠谱
+ * 注解写出来是大家用，这个是自己用，看取舍
  *
  * 参考: https://phoenixnap.com/kb/spring-boot-validation-for-rest-services
  */
