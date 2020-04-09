fac0a9b56e755c4319402d49fc44cf556feb3f07
离线请留言 <347061329@qq.com>
2020-04-09 15:33:02 +0800

    springboot 注册nacos并使用配置中心

---
+++ springboot/springboot-nacos/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ dataId：这个属性是需要在Nacos中配置的Data Id。
+ autoRefreshed：为true的话开启自动更新。
+
+ */
+@SpringBootApplication
+@NacosPropertySource(dataId = "springboot-nacos-config", autoRefreshed = true)
+public class DemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@NacosInjected
+	private NamingService namingService;
+
+	@Value("${spring.application.name}")
+	private String applicationName;
+
+	@Value("${server.port}")
+	private Integer serverPort;
+
+	@Override
+	public void run(String... args) throws Exception {
+		// 通过Naming服务注册实例到注册中心
+		namingService.registerInstance(applicationName, "127.0.0.1", serverPort);
+	}
+}
---
+++ springboot/springboot-nacos/src/main/java/com/example/demo/web/resource/NacosController.java
@@ -0,0 +1,53 @@
+package com.example.demo.web.resource;
+
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class NacosController {
+
+    /**
+     * 在nacos中配置键值对即可
+     */
+    @NacosValue(value = "${nacos.test.propertie:123}", autoRefreshed = true)
+    private String testProperties;
+
+    /**
+     * 测试nacos配置中心
+     * @return
+     */
+    @GetMapping("/test")
+    public String test(){
+        return testProperties;
+    }
+
+    @NacosInjected
+    private NamingService namingService;
+
+    @Value("${spring.application.name}")
+    private String providerApplicationName;
+
+    /**
+     * 模拟测试，直接使用同一个服务
+     * @return
+     * @throws NacosException
+     */
+    @GetMapping("/consumer/test")
+    public String consumerTest() throws NacosException {
+        // 根据服务名从注册中心获取一个健康的服务实例
+        Instance instance = namingService.selectOneHealthyInstance(providerApplicationName);
+        // 这里只是为了方便才新建RestTemplate实例
+        RestTemplate template = new RestTemplate();
+        String url = String.format("http://%s:%d/test", instance.getIp(), instance.getPort());
+        String result = template.getForObject(url, String.class);
+        System.out.println(String.format("请求URL:%s,响应结果:%s", url, result));
+        return result;
+    }
+}
