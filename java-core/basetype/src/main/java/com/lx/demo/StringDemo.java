c0da1ff7a6a818ba83e70ee91415a4ecfa8d7455
离线请留言 <347061329@qq.com>
2020-11-11 20:42:41 +0800

    补0测试

--- java-core/basetype/src/main/java/com/lx/demo/StringDemo.java
+++ java-core/basetype/src/main/java/com/lx/demo/StringDemo.java
@@ -9,6 +9,13 @@ import java.lang.reflect.Field;
  */
 public class StringDemo {
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+
+        //针对999 补0
+        String str = String.format("%6d", 999).replace(" ", "0");
+        final String format = String.format("%06d", 99);//其中0表示补零而不是补空格，6表示至少6位
+        System.out.println("补0 : " + str);
+        System.out.println("补0 : " +format);
+
         String hello = "hello ";
         String world = "world";
 
