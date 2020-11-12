076621210be5253d3c908a042f4c0b203cd56fde
离线请留言 <347061329@qq.com>
2020-11-12 15:50:43 +0800

    特殊字符清理

--- java-core/pattern/src/main/java/com/example/StringReplace.java
+++ java-core/pattern/src/main/java/com/example/StringReplace.java
@@ -5,9 +5,9 @@ import java.util.HashMap;
 public class StringReplace {
     public static void main(String[] args) {
         final HashMap<String, Object> dto = new HashMap<>();
-        dto.put("name", "%>我了歌曲</xx\\哈哈|&呵呵");
+        dto.put("name", "%>我了   歌曲</x x\\哈 哈|&呵呵");
         System.out.println("修改前: " + dto);
-        dto.put("name", String.valueOf(dto.get("name")).replaceAll("[%></\\\\|&]", ""));
+        dto.put("name", String.valueOf(dto.get("name")).replaceAll("[%></\\\\ |&]", ""));
         System.out.println("修改后: " + dto);
     }
 }
