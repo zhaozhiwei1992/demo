68c3e55d92b66ff96f24e6bcf80a4bbeba969089
离线请留言 <347061329@qq.com>
2021-04-10 16:53:49 +0800

    classloader测试

---
+++ java-core/basetype/src/main/java/com/lx/demo/FileSystemClassLoader.java
@@ -0,0 +1,56 @@
+package com.lx.demo;
+
+import java.io.*;
+
+/**
+ * @Title: FileSystemClassLoader
+ * @Package com/lx/demo/FileSystemClassLoader.java
+ * @Description: 自定义类加载器，继承自 java.lang.ClassLoader，用于加载文件系统上的类。它首先根据类的全名在文件系统上查找类的字节代码文件（.class 文件），然后读取该文件内容，最后通过
+ * defineClass() 方法来把这些字节代码转换成 java.lang.Class 类的实例。
+ *
+ * java.lang.ClassLoader 的 loadClass() 实现了双亲委派模型的逻辑，自定义类加载器一般不去重写它，但是需要重写 findClass() 方法。
+ * @author zhaozhiwei
+ * @date 2021/4/10 下午4:10
+ * @version V1.0
+ */
+public class FileSystemClassLoader extends ClassLoader {
+
+    private String rootDir;
+
+    public FileSystemClassLoader(String rootDir) {
+        this.rootDir = rootDir;
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] classData = getClassData(name);
+        if (classData == null) {
+            throw new ClassNotFoundException();
+        } else {
+            return defineClass(name, classData, 0, classData.length);
+        }
+    }
+
+    private byte[] getClassData(String className) {
+        String path = classNameToPath(className);
+        try {
+            InputStream ins = new FileInputStream(path);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int bufferSize = 4096;
+            byte[] buffer = new byte[bufferSize];
+            int bytesNumRead;
+            while ((bytesNumRead = ins.read(buffer)) != -1) {
+                baos.write(buffer, 0, bytesNumRead);
+            }
+            return baos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String classNameToPath(String className) {
+        return rootDir + File.separatorChar
+                + className.replace('.', File.separatorChar) + ".class";
+    }
+}
