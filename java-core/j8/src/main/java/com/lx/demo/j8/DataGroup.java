7a15f2933e18b1a84ea1c9531508f30c426abe57
离线请留言 <347061329@qq.com>
2021-12-16 16:48:45 +0800

    增加根据金额分组方法

--- java-core/j8/src/main/java/com/lx/demo/j8/DataGroup.java
+++ java-core/j8/src/main/java/com/lx/demo/j8/DataGroup.java
@@ -1,5 +1,6 @@
 package com.lx.demo.j8;
 
+import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -11,14 +12,17 @@ public class DataGroup {
         HashMap<String, Object> hashMap = new HashMap<>();
         hashMap.put("id", "1");
         hashMap.put("name", "zhangsan");
+        hashMap.put("amt", BigDecimal.ZERO);
 
         HashMap<String, Object> hashMap2 = new HashMap<>();
         hashMap2.put("id", "2");
         hashMap2.put("name", "zhangsan2");
+        hashMap2.put("amt", BigDecimal.ZERO);
 
         HashMap<String, Object> hashMap3 = new HashMap<>();
         hashMap3.put("id", "1");
         hashMap3.put("name", "zhangsan3");
+        hashMap3.put("amt", new BigDecimal(-1));
 
         maps.addAll(Arrays.asList(hashMap, hashMap2, hashMap3));
 
@@ -30,6 +34,10 @@ public class DataGroup {
         //j8分组
         Map<Object, List<Map>> id = maps.stream().collect(Collectors.groupingBy(map -> map.get("id")));
         System.out.println("j8 后分组结果: " + id);
+
+        // 金额分组
+        final Map<String, List<Map>> groupByAmt = groupByAmt(maps);
+        System.out.println("根据金额分组: " + groupByAmt);
     }
 
     /**
@@ -50,6 +58,40 @@ public class DataGroup {
                 List<T> list = new ArrayList<T>();
                 list.add(dataItem);
                 resultMap.put(String.valueOf(dataItem.get(packReg)), list);
+            }
+        }
+        return resultMap;
+    }
+
+    /**
+     * @data: 2021/12/16-下午4:28
+     * @User: zhaozhiwei
+     * @method: groupByAmt
+      * @param dataList :
+     * @return: java.util.Map<java.lang.String,java.util.List<T>>
+     * @Description: 根据金额, 大于0, 小于0, 等于0分组
+     */
+    public static <T extends Map> Map<String, List<T>> groupByAmt(List<T> dataList){
+        T dataItem;
+        Map<String, List<T>> resultMap = new HashMap<String, List<T>>();
+        for (T aDataList : dataList) {
+            dataItem = (T) aDataList;
+            String packReg = "";
+//            根据金额, 大于0: gt0, 小于0: lt0, 等于0:lt0分组
+            final BigDecimal curAmt = new BigDecimal(String.valueOf(dataItem.get("amt")));
+            if(curAmt.compareTo(BigDecimal.ZERO) < 0){
+                packReg = "lt0";
+            }else if(curAmt.compareTo(BigDecimal.ZERO) == 0){
+                packReg = "eq0";
+            }else{
+                packReg = "gt0";
+            }
+            if (resultMap.containsKey(packReg)) {
+                resultMap.get(packReg).add(dataItem);
+            } else {
+                List<T> list = new ArrayList<T>();
+                list.add(dataItem);
+                resultMap.put(packReg, list);
             }
         }
         return resultMap;
