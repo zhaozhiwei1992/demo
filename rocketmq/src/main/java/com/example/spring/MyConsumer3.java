bb5f3bada8f9d6283dfefe0da5fe260020fc1ab5
zhaozhiwei <347061329@qq.com>
2025-04-13 23:50:06 +0800

    fix: rocketmq测试,生产消费测试

--- rocketmq/src/main/java/com/example/spring/MyConsumer3.java
+++ rocketmq/src/main/java/com/example/spring/MyConsumer3.java
@@ -4,8 +4,16 @@ import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
 
+/**
+ * @Title: MyConsumer3
+ * @Package com/example/spring/MyConsumer3.java
+ * @Description: 注意这里是另一个消费者组TestGroup2, 不同消费者组可以消费一个queue
+ * @author zhaozhiwei
+ * @date 2025/4/13 21:54
+ * @version V1.0
+ */
 @Component
-@RocketMQMessageListener(topic = "TestTopic", consumerGroup = "TestGroup")
+@RocketMQMessageListener(topic = "TestTopic", consumerGroup = "TestGroup2")
 public class MyConsumer3 implements RocketMQListener<String> {
 
     @Override
